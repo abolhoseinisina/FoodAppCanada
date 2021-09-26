1b63abd632ea879fa81920b1f780cbd523dfae41
Ebola <abolhoseini.sina@gmail.com>
2021-09-26 19:21:10 +0330

    implement DailyMenu ApiController

--- FoodApp/Controllers/DailyMenuController.cs
+++ FoodApp/Controllers/DailyMenuController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FoodApp.Application.Interface;
+using FoodApp.Application.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace FoodApp.Controllers
 {
@@ -6,6 +9,31 @@ namespace FoodApp.Controllers
     [Route("api/[controller]")]
     public class DailyMenuController : ControllerBase
     {
+        public IServingService ServingService { get; }
 
+        public DailyMenuController(IServingService servingService)
+        {
+            ServingService = servingService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetDailyMenu([FromBody] GenderAge genderAge)
+        {
+            return Ok(await ServingService.GetDailyMenu(genderAge));
+        }
+
+        [HttpGet]
+        [Route("AgeRange")]
+        public async Task<IActionResult> GetAgeRange()
+        {
+            return Ok(await ServingService.GetAgeRanges());
+        }
+
+        [HttpGet]
+        [Route("GenderRange")]
+        public async Task<IActionResult> GetGenderRange()
+        {
+            return Ok(await ServingService.GetGenderRanges());
+        }
     }
 }
