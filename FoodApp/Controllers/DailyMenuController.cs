2a8fc7d19cdd4f06a04a21d86b8dd55ea10509fa
Ebola <abolhoseini.sina@gmail.com>
2021-09-26 13:02:03 +0330

    Add project files.

---
+++ FoodApp/Controllers/DailyMenuController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DailyMenuController : ControllerBase
+    {
+
+    }
+}
