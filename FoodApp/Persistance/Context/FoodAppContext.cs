a65c0b2a6b481835a4d3e3755d44cbda40e9d4af
Ebola <abolhoseini.sina@gmail.com>
2021-09-26 19:10:27 +0330

    add persistance layer and its dataContext and dataReader

---
+++ FoodApp/Persistance/Context/FoodAppContext.cs
@@ -0,0 +1,125 @@
+﻿using FoodApp.Domain.Model;
+using FoodApp.Persistance.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace FoodApp.Persistance.Context
+{
+    public class FoodAppContext : DbContext
+    {
+        public FoodAppContext(DbContextOptions<FoodAppContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Food> Foods { get; set; }
+
+        public DbSet<FoodGroup> FoodGroups { get; set; }
+
+        public DbSet<Serving> Servings { get; set; }
+
+        public DbSet<Statement> Statements { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=FoodAppDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            SeedData(modelBuilder);
+        }
+
+        private void SeedData(ModelBuilder modelBuilder)
+        {
+            FillServingTable(modelBuilder);
+            FillFoodTable(modelBuilder);
+            FillFoodGroupTable(modelBuilder);
+            FillStatementTable(modelBuilder);
+        }
+
+        private void FillStatementTable(ModelBuilder modelBuilder)
+        {
+            var statements = DataReader.ReadStatementCSV();
+            var statementData = new List<Statement>();
+
+            for (int i = 1; i < statements.Count; i++)
+            {
+                statementData.Add(
+                    new Statement
+                    {
+                        Id = i,
+                        FoodGroupId = statements[i][0],
+                        DirectionalStatement = statements[i][1]
+                    }
+                );
+            }
+            modelBuilder.Entity<Statement>().HasData(statementData);
+        }
+
+        private void FillFoodGroupTable(ModelBuilder modelBuilder)
+        {
+            var foodGroups = DataReader.readFoodGroupCSV();
+            var foodGroupData = new List<FoodGroup>();
+
+            for (int i = 1; i < foodGroups.Count; i++)
+            {
+                var foodGroupCategoryId = int.Parse(foodGroups[i][2]) + 1;
+                foodGroupData.Add(
+                    new FoodGroup
+                    {
+                        Id = i,
+                        FoodGroupId = foodGroups[i][0],
+                        FoodGroupName = foodGroups[i][1],
+                        FoodGroupCategoryId = foodGroupCategoryId,
+                        FoodGroupCategoryName = foodGroups[i][3]
+                    }
+                );
+            }
+            modelBuilder.Entity<FoodGroup>().HasData(foodGroupData);
+        }
+
+        private void FillFoodTable(ModelBuilder modelBuilder)
+        {
+            var foods = DataReader.readFoodCSV();
+            var foodData = new List<Food>();
+
+            for (int i = 1; i < foods.Count; i++)
+            {
+                foodData.Add(
+                    new Food
+                    {
+                        Id = i,
+                        FoodGroupId = foods[i][0],
+                        FoodGroupCategory = int.Parse(foods[i][1]),
+                        ServingSize = foods[i][2],
+                        Foods = foods[i][3]
+                    }
+                );
+            }
+            modelBuilder.Entity<Food>().HasData(foodData);
+        }
+
+        private void FillServingTable(ModelBuilder modelBuilder)
+        {
+            var servings = DataReader.readServingCSV();
+            var servingData = new List<Serving>();
+
+            for (int i = 1; i < servings.Count; i++)
+            {
+                servingData.Add(
+                    new Serving
+                    {
+                        Id = i,
+                        FoodGroupId = servings[i][0],
+                        Gender = servings[i][1],
+                        Age = servings[i][2],
+                        Servings = servings[i][3]
+                    }
+                );
+            }
+            modelBuilder.Entity<Serving>().HasData(servingData);
+        }
+    }
+}
---
+++ FoodApp/Persistance/Data/DataReader.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace FoodApp.Persistance.Data
+{
+    public class DataReader
+    {
+        public static List<string[]> readServingCSV()
+        {
+            var servingValues = new List<string[]>();
+            int lineNumber = 0;
+            using (var reader = new StreamReader(@"Persistance\Data\servings_per_day-en_ONPP.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    lineNumber++;
+                    if (lineNumber == 1)
+                        continue;
+
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    servingValues.Add(values);
+                }
+            }
+            return servingValues;
+        }
+
+        public static List<string[]> readFoodCSV()
+        {
+            var foodValues = new List<string[]>();
+            int lineNumber = 0;
+            using (var reader = new StreamReader(@"Persistance\Data\foods-en_ONPP_rev.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    lineNumber++;
+                    if (lineNumber == 1)
+                        continue;
+
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    foodValues.Add(values);
+                }
+            }
+            return foodValues;
+        }
+
+        public static List<string[]> readFoodGroupCSV()
+        {
+            var foodGroupValues = new List<string[]>();
+            int lineNumber = 0;
+            using (var reader = new StreamReader(@"Persistance\Data\foodgroups-en_ONPP.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    lineNumber++;
+                    if (lineNumber == 1)
+                        continue;
+
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    foodGroupValues.Add(values);
+                }
+            }
+            return foodGroupValues;
+        }
+
+        public static List<string[]> ReadStatementCSV()
+        {
+            var statementValues = new List<string[]>();
+            int lineNumber = 0;
+            using (var reader = new StreamReader(@"Persistance\Data\fg_directional_satements-en_ONPP.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    lineNumber++;
+                    if (lineNumber == 1)
+                        continue;
+
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    statementValues.Add(values);
+                }
+            }
+            return statementValues;
+        }
+    }
+}
